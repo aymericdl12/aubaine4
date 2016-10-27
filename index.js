2e71c68f5a0e4edd5871458d11ce2a1dc25a787e
Aymeric de Lanouvelle <aymeric.lanouvelle@gmail.com>
2016-10-27 14:00:57 +0200

    implement rest api 1

--- index.js
+++ index.js
@@ -1,10 +1,118 @@
 var express = require('express');
 var app = express();
+var path = require("path");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 var cool = require('cool-ascii-faces');
+
+var DEALS_COLLECTION = "deals";// Connection URL 
+var db_url = "mongodb://heroku_73397x48:f6cr0qggse5nb158s6nups3hu3@ds047592.mlab.com:47592/heroku_73397x48";
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(db_url, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+// DEALS API ROUTES BELOW
+
+
+/*  "/deals"
+ *    GET: finds all deals
+ *    POST: creates a new deal
+ */
+
+app.get("/deals", function(req, res) {
+  db.collection(DEALS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get deals.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/deals", function(req, res) {
+  var newDeal = req.body;
+  newDeal.createDate = new Date();
+
+  if (!(req.body.firstName || req.body.lastName)) {
+    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+  }
+
+  db.collection(DEALS_COLLECTION).insertOne(newDeal, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new deal.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+/*  "/deals/:id"
+ *    GET: find deal by id
+ *    PUT: update deal by id
+ *    DELETE: deletes deal by id
+ */
+
+app.get("/deals/:id", function(req, res) {
+  db.collection(DEALS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get deal");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
+app.put("/deals/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(DEALS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update deal");
+    } else {
+      res.status(204).end();
+    }
+  });
+});
+
+app.delete("/deals/:id", function(req, res) {
+  db.collection(DEALS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete deal");
+    } else {
+      res.status(204).end();
+    }
+  });
+});
+
+
+
+
+// TO BE REMOVED
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -18,31 +126,29 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-  console.log('coucou github');
-});
+// app.get('/cool', function(request, response) {
+//   response.send(cool());
+//   console.log('coucou github');
+// });
 
-app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < 60; i++)
-      result += i + ' ';
-  response.send(result);
-});
+// app.get('/times', function(request, response) {
+//     var result = ''
+//     var times = process.env.TIMES || 5
+//     for (i=0; i < 60; i++)
+//       result += i + ' ';
+//   response.send(result);
+// });
 
-app.get('/db', function(request, response) {
-	var MongoClient = require('mongodb').MongoClient;
-	var assert = require('assert');
-	// Connection URL 
-	var url = "mongodb://heroku_73397x48:f6cr0qggse5nb158s6nups3hu3@ds047592.mlab.com:47592/heroku_73397x48";
-	// Use connect method to connect to the Server 
-	MongoClient.connect(url, function(err, db) {
-		assert.equal(null, err);
-		console.log("Connected correctly to server");
-		db.close();
-		response.send(cool());
-	});
+// app.get('/db', function(request, response) {
+// 	var MongoClient = require('mongodb').MongoClient;
+// 	var assert = require('assert');
+	
+// 	// Use connect method to connect to the Server 
+// 	MongoClient.connect(url, function(err, db) {
+// 		assert.equal(null, err);
+// 		console.log("Connected correctly to server");
+// 		db.close();
+// 		response.send(cool());
+// 	});
+// });
 
-
-});
